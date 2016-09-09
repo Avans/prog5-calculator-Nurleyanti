7a5fca9d577cd8d7aaae1133e1c0915ca78037fb
Nurleyanti <nsakidin@avans.nl>
2016-09-09 16:40:42 +0200

    final commit

---
+++ Calculator/AgeCal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class AgeCal : IAgeCalculator
+    {
+        public int CalculateAge(DateTime dateOfBirth)
+        {
+            return (DateTime.Today - dateOfBirth).Days / 365;
+        }
+
+
+public bool ParseInput(string input, out DateTime result)
+        {
+            
+            if(DateTime.TryParse(input, out result))
+            {if (input != null)
+                {
+                    return true;
+                }
+            }return false;
+        }
+    }
+}
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -10,8 +10,28 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Success!");
-            Console.ReadLine();
+            Boolean check = true;
+            while (check)
+            {
+                Console.WriteLine("Enter your date of birth (dd-mm-yyyy)");
+                String input = Console.ReadLine();
+                if(input == "end" || input == "quit") { break; }
+                AgeCal ageCal = new AgeCal();
+                DateTime result;
+                if (ageCal.ParseInput(input, out result))
+                {
+                    int age = ageCal.CalculateAge(result);
+                    Console.WriteLine("Your age: " + age);
+                    check = false;
+
+                }
+                else
+                {
+                    Console.WriteLine("Please enter your date of birth like we want to");
+                }
+
+                
+            }Console.ReadLine();
         }
     }
 }
